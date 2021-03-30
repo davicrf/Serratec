02a813b3f9416cd89724d398998ff964f3b8f344
DanieldSantosFonseca <danielsfonseca@outlook.com>
2021-03-30 17:57:53 -0300

    Grupo 2 Lógica de Programação com JS - Nutrição

---
+++ nutricaoGrupo2/leia-me.md
@@ -0,0 +1,121 @@
+# Calorias sob medida
+<p> oi <p>
+!Saber quantas calorias o corpo gasta para manter as funções vitais, o chamado metabolismo basal, é o primeiro passo para adequar a alimentação ao gasto energético diário. Não se trata de tornar-se escravo da contagem de calorias, mas de uma informação relevante para os indivíduos saudáveis e fisicamente ativos que querem emagrecer ou manter o peso corporal.
+===========================================================================================================
+Existem duas formulas mais adequada para o calculo de necessidade energética diária. Uma delas é a Harris Benedict, que fornece o metabolismo basal. Apesar de ser vovó nas equações (é de 1919), ela é ainda hoje uma das prediletas dos nutricionistas porque não superestima as calorias. Além disso, seu resultado pode ser pelo fator de atividade física, no caso dos fisicamente ativos, e somados à ação dinâmica especifica (gasto adicional de energia para a digestão de alimentos), quando a alimentação é fracionada a cada três horas.
+==============================
+A segunda formula para o cálculo é a Taxa Metabólica Basal (TMB), que também pode ser multiplicada por um fator de atividade física, de acordo com a intensidade do exercício.
+
+A nutricionista do PA Club, Patricia Campos, doutora Bioquímica pela Unicamp, lembra que as pessoas fisicamente ativas podem ser mais pesadas na balança em um determinado período, o que não necessariamente implica que elas tenham engordado. “O peso é apenas uma unidade de medida. Se a pessoa estiver mudando a composição corporal, ou seja, trocando gordura por músculos, pode ficar mais pesada, mas não esta engordando”, explica. Por isso, ela recomenda que os corredores em fase de manutenção do peso façam avaliações corporais (com professores de educação física ou nutricionistas) a cada três meses. Já as pessoas que estão fazendo dietas para a perda de peso devem adotar avaliações mensais,  segundo a Patricia.
+
+
+Confira, a seguir, como usar as formulas para calcular a ingestão calórica diária:
+FÓRMULA 1
+1) HARIS BENEDICT: Esta fórmula é usada para obter o gasto calórico basal diário. A unidade para peso é o quilograma, a altura, os centímetros, a idade e os anos.
+
+# Homens: 66 + (13,7 x Peso) + (5,0 x Altura) – (6,8 x Idade)
+
+
+Mulheres:
+665 + (9,6 x Peso) + (1,8 x Altura) – (4,7 x Idade)
+
+Como calcular: Para um homem de 70 kg, 1,80 metros de altura, 30 anos, isso equivale a 1.721 calorias de metabolismo basal. Uma mulher de mesma idade, 55 kg e 1,60 metros de altura gasta em seu metabolismo basal 1.330 calorias.
+
+2) FATOR ATIVIDADE FÍSICA: Para saber o total de calorias que o
+individuo deve ingerir, é preciso considerar quanto ele gasta por
+minuto em determinado esporte, de acordo com o seu peso corporal
+e intensidade do exercício. Vale ressaltar, no entanto, que esse é um
+calculo estimado e que o gasto por minuto em corrida pode variar de
+individuo par individuo. Os valores estão abaixo:
+Tempo para percorrer um quilometro Gasto calórico por
+minuto
+7 min 14 s/km 9,6 cal/min
+6 min /km 13,7 cal/min
+5 min/km 14,8 cal/min
+4 min 30 s/km 16,2 cal/min
+4 min 12 s/km 17,9 cal/min
+3 min 40 s/km 20,5 cal/min
+Como calcular: A ingestão calórica do individuo é dada por intermédio
+do calculo da fórmula de Harris Benedict (metabolismo basal), cujo
+resultado deve ser somado pelo gasto estabelecido pela atividade
+física por minuto (fator de atividade = tempo x calorias por minuto).
+Um indivíduo que corre uma hora por dia numa velocidade de 5
+min/km gasta: 60 (minutos) x 14,8 cal (calorias gastas por minuto) =
+888 cal, que serão somadas ao metabolismo basal. A necessidade
+energética deste corredor é dada pela soma do resultado da Harris
+Benedict (1.721 cal) com o fator de atividade física (888 cal),
+chegando a 2.609 cal ao dia. Já a mulher que corre 60 minutos a 6
+min/km gasta: 60 (minutos) x 13,7 cal (calorias gastas por minuto) =
+822 cal. A soma da Harris Benedict (1.330 cal) com o fator de
+atividade física (822 cal), chegando a 2.152 cal/dia.
+3) ALIMENTAÇÃO FRACIONADA. Os que adotam a alimentação
+fracionada podem acrescentar também a ação dinâmica especifica
+que, em media, corresponde a 10% do metabolismo basal, calculada
+por meio da fórmula de Harris Benedict:
+Fórmula: Valor Calórico Total (VCT) = Harris Benedict + ADE (10%
+de Harris Benedict) + AF (fator atividade física).
+Revista: O2 www.attitudesports.com.br
+Julho/06 
+Como calcular: Para o corredor de 70 kg já citado anteriormente, o
+VCT = 1.721 cal + 172 cal + 888 cal = 2.781 cal/dia. Para a mulher,
+o CVT é de 2.285 cal ao dia.
+FÓRMULA 2
+TAXA METÁBOLICA BASAL (TMB)
+# 1) Use a fórmula abaixo para calcular a taxa metabólica basal. O peso corporal deve ser colocado em quilos.
+
+Homens Mulheres
+# 18-30 anos 15,3 x peso + 679 14,7 x peso +496
+
+# 31-60 anos 11,6 x peso + 879 8,7 x peso + 829
+
+# Acima de 60 anos 13,5 x peso + 487 10,5 x peso + 596
+
+Como calcular: para a mulher de 30 anos e 55 kg isso corresponde a
+(14,7 x 55 + 496) = 1.304 calorias para o metabolismo basal. O
+homem da mesma idade e 70 kg tem gasto basal de 1.750 calorias.
+
+2) A TMB deve ser multiplicada pelo fator atividade (tabela acima), de
+acordo com a característica da atividade física diária da pessoa, para
+corresponder à necessidade energética diária total da mesma.
+FAVOR ATIVIDADE
+ Leve Moderada Intensa
+Homens 1,55 1,78 2,10
+Mulheres 1,56 1,64 1,82
+Como calcular: para a personagem já citada, a TMB é de 1.304 cal,
+valor que é multiplicado pelo fator de atividade física moderada
+(1,64), num total de 2.138 cal ao dia. Já o corredor com gasto basal
+de 1.750 cal e atividade física moderada (X 1,78) pode ingerir, pela
+fórmula, 3.115 cal por dia.
+Planejamento nutricional
+Depois de fazer os cálculos, o corredor deve verificar se sua ingestão
+esta adequada ao seu gasto, e de acordo com os seus objetivos,
+traçar uma estratégia nutricional. Se o objetivo for manter peso, o
+gasto deve ser o mesmo da ingestão. Se a idéia for emagrecer, a
+ingestão deve ser menor do que o gasto, mas há limites para que
+isso seja feito de forma saudável. “Para indivíduos saudáveis e que
+praticam atividade física regularmente, eu recomendo a subtração de
+300 cal a 500 cal por dia, e a consulta com um nutricionista”, afirma
+Heloísa Guarita, nutricionista especializada em fisiologia do exercício
+Revista: O2 www.attitudesports.com.br
+Julho/06 
+pela Unifesp (escola paulista de Medicina), sócia-diretora da RG Nutri
+Consultoria Nutricional, nutricionista do Projeto Mulher e
+coordenadora do Departamento de nutrição da Academia Reebok de
+São Paulo.
+A necessidade calórica diária deve ser dividida entre os três grupos
+de alimentos. “De uma maneira geral as proteínas ficam com 15% a
+20% das calorias, os carboidratos com 55% a 60% e as gorduras
+com 20% a 25%. Depois disso, deve-se distribuir as calorias nas
+cinco ou seis refeições do dia. O ideal é que cada uma das grandes
+refeições contenha carboidrato, gordura e proteína” explica Heloísa
+Guarita.
+De acordo com Heloísa, um exemplo de boa distribuição entre as
+refeições seria: café da manhã com 20% das calorias diárias, o
+lanche da manhã com 5%, o almoço com 30% a 35%, o lanche da
+tarde com 19% a 15% e, o jantar com 20% a 25%. Se houver saldo
+o lanche a noite pode englobar os 15% restantes. “Um fator que
+influencia na organização das calorias é o treino. Se ele for de
+manhã, eu recomendo que ao acordar o corredor coma um lanche
+que equivalha a 5% de sua ingestão calórica diária. E que ele faça o
+seu café da manhã depois do treino. Se for à noite, deve lanchar
+antes de correr e, depois jantar”, exemplifica Heloísa. 
---
+++ nutricaoGrupo2/script.js
@@ -0,0 +1,259 @@
+//ARMAZERNAR INFORMAÇÕES
+
+let usuario = [
+    {
+        nome: "",
+        idade: 0,
+        peso: 0,
+        altura: 0,
+        sexo: "",
+        emagrecimento: 0,
+        imc: 0,
+        gebHomem: 0,
+        gebMulher: 0,
+        caloriaTotal: 0,
+        get: 0,
+        nivelAtividade: 0,
+        diasParaEmagrecer: 0,
+    },
+];
+
+//TABELA GAF
+
+/*
+    var gaf = [
+        {
+            sedentario: 1.2,
+            poucoAtivo: 1.375,
+            moderado: 1.55,
+            intenso: 1.725,
+            muitoIntenso: 1.9
+        }
+    ];
+*/
+
+const gaf = [
+    {
+        sedentario: 1.15,
+        poucoAtivo: 1.45,
+        moderado: 1.75,
+        intenso: 2.05,
+        muitoIntenso: 2.35,
+    },
+];
+
+//FUNÇÃO PARA CALCULA VALORES
+
+function calcular() {
+
+    // VARIAVEIS -- PEGAR INFORMAÇÕES E VALIDAR
+
+    let nome = prompt("Digite seu nome");
+
+    while (nome == null) {
+        nome = prompt("Por favor, digite seu nome");
+    }
+
+    /* 
+        Parâmetros da função:
+        1) "X" --> Valor digitado pelo usuário
+        2) "Y" --> Função de conversão para número inteiro ou real
+        3) "Z" --> Pergunta que deverá realizar ao usuário.
+    */
+
+    let idade = validarNumero(parseInt(prompt("Informe sua idade")), parseInt, "Por favor, informe sua idade");
+
+    let peso = validarNumero(parseFloat(prompt("Informe seu peso em kilos(KG)")), parseFloat, "Por favor, informe seu peso em kilos(KG)");
+
+    let altura = validarNumero(parseFloat(prompt("Informe sua altura em metros(m)")), parseFloat, "Por favor, informe sua altura em metros(m)");
+
+    let emagrecimento = parseFloat(
+        prompt("Informe quantos quilos pretende emagrecer: ")
+    );
+
+    while (isNaN(emagrecimento)) {
+        emagrecimento = parseFloat(
+            prompt("Informe quantos quilos pretende emagrecer:")
+        );
+    }
+
+    function validarNumero(x, y, z) {
+        while (isNaN(x)) {
+            x = y(prompt(`${z}`));
+        }
+        return x;
+    }
+
+    /* 
+        Parâmetros da função:
+        1) "X" --> Sexo digitado pelo usuário
+        2) "Y" --> Valor booleano para validação
+    */
+
+    let sexo = validarSexo(prompt("Informe seu sexo: \n Digite \"M\" para masculino \n Digite \"F\" para feminino").toLowerCase(), true);
+
+    function validarSexo(x, z) {
+        while (z) {
+            if (x == "m" || x == "f") {
+                z = false
+            } else {
+                x = prompt("Por favor, informe seu sexo: \n Digite \"M\" para masculino \n Digite \"F\" para feminino").toLowerCase();
+            }
+        }
+        return x
+    }
+
+    
+
+    let nivelAtividade = parseInt(
+        prompt(
+            "Digite de 1 a 5 o quão ativo você é, sendo: 1 =  a sendatario e 5 muito ativo"
+        )
+    );
+    let valorNivelAtividade = true;
+
+    while (valorNivelAtividade == true) {
+        if (!isNaN(nivelAtividade) && nivelAtividade >= 1 && nivelAtividade <= 5) {
+            valorNivelAtividade = false;
+        } else {
+            nivelAtividade = parseInt(
+                prompt(
+                    "Digite de 1 a 5 o quão ativo você é, sendo: 1 =  a sendatario e 5 muito ativo"
+                )
+            );
+            valorNivelAtividade = true;
+        }
+    }
+
+    // VARIAVES COM VALOR DOS CALCULOS + CALCULOS E CONDIÇÕES
+
+    let caloriaTotal = 7700 * emagrecimento;
+    let imc = peso / (altura * altura);
+    let gebHomem = parseInt(66.47 + 13.75 * peso + 5 * altura + 6.76 * idade);
+    let gebMulher = parseInt(655.1 + 9.56 * peso + 1.85 * altura + 4.68 * idade);
+    let pesoIdeal = 21.75 * altura * altura;
+    let get = 0;
+    let diasParaEmagrecer = 0;
+    let _1KgPorSemana = "";
+
+    // CONDIÇÕES
+
+    if (sexo == "masculino") {
+        switch (nivelAtividade) {
+            case 1:
+                get = gebHomem * gaf[0].sedentario;
+                break;
+            case 2:
+                get = gebHomem * gaf[0].poucoAtivo;
+                break;
+            case 3:
+                get = gebHomem * gaf[0].moderado;
+                break;
+            case 4:
+                get = gebHomem * gaf[0].intenso;
+                break;
+            case 5:
+                get = gebHomem * gaf[0].muitoIntenso;
+                break;
+        }
+    } else {
+        switch (nivelAtividade) {
+            case 1:
+                get = gebMulher * gaf[0].sedentario;
+                break;
+            case 2:
+                get = gebMulher * gaf[0].poucoAtivo;
+                break;
+            case 3:
+                get = gebMulher * gaf[0].moderado;
+                break;
+            case 4:
+                get = gebMulher * gaf[0].intenso;
+                break;
+            case 5:
+                get = gebMulher * gaf[0].muitoIntenso;
+                break;
+        }
+    }
+
+    /* TEMPO PARA PERDER PESO */
+
+    diasParaEmagrecer = caloriaTotal / 1100;
+
+    if (sexo == "masculino") {
+        if (get - gebHomem < 1100) {
+            _1KgPorSemana =
+                "Você terá que fazer mais exercícios para conseguir emagrecer 1kg por semana";
+        } else {
+            _1KgPorSemana = `
+                Ingerindo no máximo ${parseInt(
+                get - 1100
+            )} Calorias por dia, você conseguira emagrecer 1 kg por semana <br>
+                ${parseInt(diasParaEmagrecer)} dias para emagrecer <br>
+            `;
+        }
+    } else {
+        if (get - gebMulher < 1100) {
+            _1KgPorSemana =
+                "Você terá que fazer mais exercícios para conseguir emagrecer 1kg por semana";
+        } else {
+            _1KgPorSemana = `
+                Ingerindo no máximo ${parseInt(
+                get - 1100
+            )} Calorias por dia, você conseguira emagrecer 1 kg por semana <br>
+                ${parseInt(diasParaEmagrecer)} dias para emagrecer <br>
+            `;
+        }
+    }
+
+    // ENVIAR INFORMAÇÕES PARA A ARRAY
+
+    usuario[0].nome = nome;
+    usuario[0].idade = idade;
+    usuario[0].peso = peso;
+    usuario[0].altura = altura;
+    usuario[0].sexo = sexo;
+    usuario[0].emagrecimento = emagrecimento;
+    usuario[0].imc = imc;
+    usuario[0].gebHomem = gebHomem;
+    usuario[0].gebMulher = gebMulher;
+    usuario[0].caloriaTotal = caloriaTotal;
+    usuario[0].pesoIdeal = pesoIdeal;
+    usuario[0].get = get;
+    usuario[0].nivelAtividade = nivelAtividade;
+    usuario[0].diasParaEmagrecer = diasParaEmagrecer;
+
+    // MOSTRAR RESPOSTAS
+
+    if (sexo == "m") {
+        document.getElementById("resposta").innerHTML = `
+                <p>Olá, ${nome}!<br>
+                O seu IMC é: ${imc.toFixed(2).replace(".",",")} <br>
+                O seu Peso ideal é: ${pesoIdeal.toFixed(2).replace(".",",")} kg<br>
+                O seu GEB é: ${gebHomem.toFixed(0)}<br>
+                você terá que gastar ${caloriaTotal} calorias para perder os ${emagrecimento.toFixed(
+            2
+        )} kg<br>
+                Seu gasto calórico diário é igual a ${parseInt(
+            get
+        )} calorias por dia<br>
+                ${_1KgPorSemana} <br>
+                </p>
+            `;
+    } else if (sexo == "f") {
+        document.getElementById("resposta").innerHTML = `
+            <p>Olá, ${nome}!<br>
+            O seu IMC é: ${imc.toFixed(2)} <br>
+            O seu Peso ideal é: ${pesoIdeal.toFixed(2)} kg<br>
+            O seu GEB é: ${gebMulher.toFixed(0)}<br>
+            você terá que gastar ${caloriaTotal} calorias para perder os ${emagrecimento.toFixed(
+            2
+        )} kg<br>
+            Seu gasto calórico diário é igual a ${parseInt(
+            get
+        )} calorias por dia<br>
+            ${_1KgPorSemana} <br>
+            </p>
+        `;
+    }
+}
